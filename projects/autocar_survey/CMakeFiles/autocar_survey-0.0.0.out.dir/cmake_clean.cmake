27f0f91d05ab99c200d864acd0ee9ac9a9c03b1e
dwkang0 <dwkang0@gmail.com>
2019-06-08 15:28:10 +0900

    .

---
+++ projects/autocar_survey/CMakeFiles/autocar_survey-0.0.0.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/autocar_survey-0.0.0.out.dir/main.cpp.o"
+  "Debug/autocar_survey-0.0.0.out.pdb"
+  "Debug/autocar_survey-0.0.0.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/autocar_survey-0.0.0.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ projects/autocar_survey/CMakeFiles/autocar_survey-0.0.0.out.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for autocar_survey-0.0.0.out.
+# This may be replaced when dependencies are built.
