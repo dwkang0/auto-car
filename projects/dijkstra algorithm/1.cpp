16960643396bc138f94be4c7157d493cffd37a0f
172635 <hjmath04@naver.com>
2019-07-28 13:18:29 +0900

    download

--- projects/dijkstra algorithm/1.cpp
+++ projects/dijkstra algorithm/1.cpp
@@ -15,30 +15,31 @@ input * file_output();
 void dijkstra_first(input *);
 int main()
 {
-    input * data=file_output();//int (* road)[V] = (int (*)[V])data; //int road[V][V]; 연결 되지 않은 길 : -1, 나머지 연결된 길 : 두 점 사이의 거리 // 다익스트라를 이용할 그래프
-    state Start_ST, End_ST;
-    Start_ST.carV=(int *)malloc(4*data->C);
-    Start_ST.carFV=(int *)malloc(4*data->C);
-    Start_ST.carT=(double *)malloc(8*data->C);
-    End_ST.carV=(int *)malloc(4*data->C);
-    End_ST.carFV=(int *)malloc(4*data->C);
-    End_ST.carT=(double *)malloc(8*data->C);
+	heap<state::car_data> a(10);
+	a.push(state::car_data(7, 2));
+	a.push(state::car_data(5, 2));
+	a.push(state::car_data(10, 3));
+	printf("%d\n", a.top().car_FT); a.pop();
+	printf("%d\n", a.top().car_FT); a.pop();
+	printf("%d\n", a.top().car_FT); a.pop();
 
-    Start_ST.carN=data->C;
-    Start_ST.nowT=0.0;
-    End_ST.carN=data->C;
-    Start_ST.nowT=(double)INF;
+    /*input * data=file_output();
+    state Start_ST(data->C), End_ST(data->C);
+
+    End_ST.carN=0;
+    End_ST.nowT=INF;
 
     for(int car_n=0;car_n<data->C;car_n++){
         Start_ST.carV[car_n]=data->car_road[car_n][0];
         Start_ST.carFV[car_n]=data->car_road[car_n][0];
-        Start_ST.carT[car_n]=0.0;
-        Start_ST.car_FT.push(state::car_data(0.0, car_n));
-        End_ST.carFV=data->car_road[car_n][1];
+        Start_ST.carT[car_n]=0;
+        Start_ST.car_FT.push(state::car_data(0, car_n));
+        End_ST.carFV[car_n]=data->car_road[car_n][1];
     }
-    Start_ST.data=data;
-    End_ST.data=data;
-    Astar<state> astar = Astar<state>(sizeof(state), Start_ST, End_ST, state::nexti, state::nextsize, state::h);
+    state::data=data;
+    Astar<state> astar = Astar<state>(Start_ST, End_ST, state::nexti, state::nextsize, state::h);
+    int time=astar.findpath();
+    printf("%d", time);*/
     return 0;
 }
 
