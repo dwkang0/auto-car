751deb6c7c58d444e281174782e4bfe4b7072b50
dwkang0 <dwkang0@gmail.com>
2019-07-29 20:29:24 +0900

    .

--- projects/eclipse_workspace_kang/ttt/src/ttt.cpp
+++ projects/eclipse_workspace_kang/ttt/src/ttt.cpp
@@ -20,21 +20,43 @@ struct roadcmp{
 };
 
 int main() {
-	heap<pair<state, int>, roadcmp> q(100);
-	pair<state, int> a(3, 2);
-	a.first.car_FT.push(state::car_data(1, 2));
-	a.first.car_FT.push(state::car_data(2, 3));
-	a.first.car_FT.push(state::car_data(-1, 2));
-	a.first.ts=10;
-	q.push(a);
-	state b(a.first);
-	printf("%d, %d\n", a.first.ts, q.top().first.ts);
-	printf("%d, %d\n", a.first.car_FT.index[2],q.top().first.car_FT.index[2]);
-//	printf("%d\n", a.first.car_FT.index[1]);
-//	printf("%d\n", a.first.car_FT.top().car_FT);  a.first.car_FT.pop();
-//	printf("%d\n", a.first.car_FT.top().car_FT);  a.first.car_FT.pop();
-//	printf("%d\n", a.first.car_FT.top().car_FT);  a.first.car_FT.pop();
-//	q.push(state(0));
+//	heap<pair<state, int>, roadcmp> q(100);
+//	pair<state, int> a(3, 2);
+//	a.first.car_FT.push(state::car_data(1, 2));
+//	a.first.car_FT.push(state::car_data(2, 3));
+//	a.first.car_FT.push(state::car_data(-1, 2));
+//	a.first.ts=10;
+//	q.push(a);
+//	state b(a.first);
+//	printf("%d, %d\n", a.first.ts, q.top().first.ts);
+//	printf("%d, %d\n", a.first.car_FT.index[2],q.top().first.car_FT.index[2]);
+////	printf("%d\n", a.first.car_FT.index[1]);
+////	printf("%d\n", a.first.car_FT.top().car_FT);  a.first.car_FT.pop();
+////	printf("%d\n", a.first.car_FT.top().car_FT);  a.first.car_FT.pop();
+////	printf("%d\n", a.first.car_FT.top().car_FT);  a.first.car_FT.pop();
+////	q.push(state(0));
 
+	heap<int> a(100);
+
+	a.push(10);
+	a.push(9);
+	a.push(15);
+	a.push(15);
+	a.push(24);
+	a.push(1);
+	a.push(30);
+	a.push(2);
+
+//	a.pop();
+//	a.pop();
+//	a.pop();
+//	a.pop();
+//	a.pop();
+//	a.pop();
+//	a.pop();
+//	a.pop();
+	while(a.heapsize!=0){
+		printf("%d\n", a.top()); a.pop();
+	}
 	return 0;
 }
