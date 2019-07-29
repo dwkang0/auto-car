bd3ce59a26338e53db46209f3346e8ee936d17e9
dwkang0 <dwkang0@gmail.com>
2019-07-29 13:43:39 +0900

    fixing...

---
+++ projects/eclipse_workspace_kang/autocar_master/src/astar.h
@@ -0,0 +1,153 @@
+#ifndef _ASTAR_H_
+#define _ASTAR_H_
+
+//HEADER START
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <memory.h>
+#include <malloc.h>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <queue>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+
+#include "log.h"
+class state;
+
+#include "state.h"
+#include "input.h"
+
+using namespace std;
+
+//HEADER END
+
+struct turn{
+    int *data_link;
+    int V;
+    turn (int C, int V):\
+        data_link((int *)malloc(4*C*V)), V(V)\
+        {
+            memset(data_link, -1, sizeof(int)*C*V);
+        }
+
+    int &link(int i, int j){
+        return data_link[i*V+j];
+    }
+};
+
+//CLASS START
+template <typename VT>
+class Astar{
+public:
+	typedef pair<VT, int> road; // first: to, second: cost
+
+	VT *startV;
+	VT *endV;
+
+	int (*nextsize)(VT &now);
+	road& (*nexti)(VT &now, int i);
+	int (*h)(VT &now, VT &end);
+	input * data;
+
+	Astar(VT &startV, VT &endV, \
+			road& (*nexti)(VT &now, int i),int (*nextsize)(VT &now), \
+			int (*h)(VT &now, VT &end), input *data):\
+			startV((VT*)malloc(sizeof(VT))), endV((VT*)malloc(sizeof(VT))),nexti(nexti), nextsize(nextsize), h(h), data((input*)malloc(sizeof(input)))\
+			{
+		log("Astar():");
+		memcpy(this->startV, &startV, sizeof(VT));
+		memcpy(this->endV, &endV, sizeof(VT));
+		memcpy(&this->data, &data, sizeof(input));
+
+		log("index,,%d",this->startV->car_FT.index[1]);
+		log("Astar() end\n");
+			}
+	~Astar(){
+		free(startV);
+		free(endV);
+	}
+
+	road findpath(turn *);
+	struct roadcmp{
+		bool operator()(const road & a, const road & b) const
+		{
+			return a.second > b.second;
+		}
+	};
+
+//	struct hashVT {
+//	    std::size_t operator()(const VT & a) const{
+//	        std::size_t h = 0;
+//	        for (int i= 0; i<astar::VTsize; i++) {
+//	        	h ^= std::hash<int>{}(((unsigned char *)&a)[i])  + 0x9e3779b9 + (h << 6) + (h >> 2);
+//	        }
+//	        return h;
+//	    }
+//	};
+};
+//CLASS END
+
+//FUNCTION START
+template <typename VT>
+
+typename Astar<VT>::road Astar<VT>::findpath(turn * turn_data){
+	log("in findPath:");
+	unordered_map<VT, int> dis;
+	unordered_set<VT> visit;
+//	priority_queue<road, vector<road>, Astar::roadcmp > q;
+	heap<road, Astar::roadcmp > q(1000000);
+	log("in findpath: index[1]:%d",startV->car_FT.index[1]);
+
+	dis.insert(road(*startV, 0));
+	q.push(road(*startV, 0));
+	road now, next;
+
+	int i;
+	while(q.heapsize){
+		now = q.top(); q.pop();
+		log("inwhile: %p, index[1]: %d",&now.first, now.first.car_FT.index[1]);
+		if(visit.find(now.first) != visit.end()) continue;
+		if(now.first == *endV){
+			return now;
+		}
+        now.first.printstate();
+		visit.insert(now.first);
+
+		for(i=0; i<nextsize(now.first); i++){
+			next = nexti(now.first, i);
+			printf("add next to queue:\n");
+//			next.first.printstate();
+			auto iter = dis.find(next.first);
+			if(iter==dis.end()){
+                int carnumber;
+                for(carnumber=0;carnumber<data->C;carnumber++){
+                    if(next.first.carT[carnumber]==next.first.nowT)
+                        break;
+                }
+                turn_data->link(carnumber, next.first.carFV[carnumber])=next.first.carV[carnumber];
+				dis.insert(road(next.first, now.second+next.second));
+				q.push(road(next.first, now.second+next.second+h(next.first, *endV)));
+			}else{
+				if(iter->second > now.second+next.second){
+				    int carnumber;
+                    for(carnumber=0;carnumber<data->C;carnumber++){
+                        if(next.first.carT[carnumber]==next.first.nowT)
+                            break;
+                    }
+                    turn_data->link(carnumber, next.first.carFV[carnumber])=next.first.carV[carnumber];
+					iter->second = now.second+next.second;
+					q.push(road(next.first, now.second+next.second+h(next.first, *endV)));
+				}
+			}
+		}
+	}
+	return road(*startV, -1);
+}
+//FUNCTION END
+#endif
---
+++ projects/eclipse_workspace_kang/autocar_master/src/autocar.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <queue>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+#include "input.h"
+#include "heap.h"
+#include "state.h"
+#include "astar.h"
+
+input * file_output();
+//void dijkstra_first(input *);
+int main()
+{
+	log("startProgram");
+
+	input * data=file_output();
+	log("inputEND\n");
+	//    printf("dd");
+	state Start_ST(data->C), End_ST(data->C);
+
+	End_ST.carN=0;
+	End_ST.nowT=INF;
+	//    printf("asd");
+
+	for(int car_n=0;car_n<data->C;car_n++){
+		Start_ST.carV[car_n]=data->car_road[car_n][0];
+		Start_ST.carFV[car_n]=data->car_road[car_n][0];
+		Start_ST.carT[car_n]=0;
+		Start_ST.car_FT.push(state::car_data(0, car_n));
+		End_ST.carFV[car_n]=data->car_road[car_n][1];
+	}
+	log("start.index[1]:%d", Start_ST.car_FT.index[1]);
+//	log("%p,%p,%p,%p",&Start_ST.car_FT.index, &Start_ST, &End_ST.car_FT.index, &End_ST);
+//	log("size:%d",sizeof(state));
+//	log("%p| start_ST.index... :%p",&Start_ST, &Start_ST.car_FT.index);
+
+	state::data=data;
+
+//	log("%p| start_ST.index... :%d\n",&Start_ST, Start_ST.car_FT.index[1]);
+//	log("%p| start_ST.index... :%d\n",&Start_ST, Start_ST.car_FT.index[1]);
+//	log("%p| size:%d, carN:%d, index,,%d\n",index,sizeof(state), s->carN, Start_ST.car_FT.index[1]);
+
+	Astar<state> astar = Astar<state>(Start_ST, End_ST, state::nexti, state::nextsize, state::h, data);
+//	log("%p| start_ST.index... :%d\n",&Start_ST, Start_ST.car_FT.index[1]);
+
+    	turn * turn_data=new turn(data->C, data->V);
+    	Astar<state>::road result=astar.findpath(turn_data);
+    	printf("%d", result.second);
+
+	log("endProgram");
+	return 0;
+}
+
